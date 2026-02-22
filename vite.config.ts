acd7eac49aef264c175ae7428b6379f14bdbce93
Matt McDonald <majmcdonald@gmail.com>
2026-02-22 09:29:04 -0600

    security fixes + base url

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
+  base: process.env.BASE_PATH || '/',
   plugins: [react(), tailwindcss()],
 })
